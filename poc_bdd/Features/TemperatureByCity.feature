e40442c98c215830fddb6f006340a7597af0c5a3
abhilabs <abhilashs@DESKTOP-HL1SOMP>
2020-10-01 15:54:44 +0530

    changed default url to swagger

---
+++ poc_bdd/Features/TemperatureByCity.feature
@@ -0,0 +1,9 @@
+﻿Feature: TemperatureByCity
+	In order to get temperature of a city	
+	I want to be told to write an api 
+
+@mytag
+Scenario: Get the temperature by city
+	Given the city name as "bangalore"
+	When a POST request to /api/Weather/temperatureByCity?city={city}
+	Then the response StatusCode should be 200
---
+++ poc_bdd/Steps/TemperatureByCitySteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace poc_bdd.Steps
+{
+    [Binding]
+    public sealed class TemperatureByCitySteps
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public TemperatureByCitySteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the city name as ""(.*)""")]
+        public void GivenTheCityNameAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"a POST request to /api/Weather/temperatureByCity\?city=\{city}")]
+        public void WhenAPOSTRequestToApiWeatherTemperatureByCityCityCity()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the response StatusCode should be (.*)")]
+        public void ThenTheResponseStatusCodeShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+    }
+}
